b349313a78f33090b57a245408134455fa9f162f
Jason Kao <jasonkao85@gmail.com>
2017-12-01 16:54:21 -0500

    inverse captcha pt 2

--- InverseCaptcha.java
+++ InverseCaptcha.java
@@ -1,24 +1,26 @@
-import java.util.*;
-import java.io.*;
-
 public class InverseCaptcha {
 	public static int inverse(String num)
 	{
 		int i, sum;
 		i = sum = 0;
 		while (i < num.length()) {
-			if (num.charAt(i) == num.charAt( (i + 1) % num.length()) {
+			if (num.charAt(i) == num.charAt( (i + 1) % num.length() )) {
 				sum += num.charAt(i) - '0';
 			}
 			i++;
 		}
 		return sum;
 	}
-
-    public static void main(String[] args)
+	public static int circularInverse(String num)
 	{
-		Scanner in = new Scanner( new BufferedReader( new InputStreamReader( System.in )));
-		long inputNum = in.next();
-		System.out.println(inverse(inputNum));
+		int i, sum;
+		i = sum = 0;
+		while (i < num.length()) {
+			if (num.charAt(i) == num.charAt( (i + num.length() / 2) % num.length() )) {
+				sum += num.charAt(i) - '0';
+			}
+			i++;
+		}
+		return sum;
 	}
 }

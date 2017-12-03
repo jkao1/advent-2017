0da8157bd0d18eeb8b7c133198b5b51c35da14e4
Jason Kao <jasonkao85@gmail.com>
2017-12-03 12:20:11 -0500

    completed day3 pt 1

--- 03/Driver.java
+++ 03/Driver.java
@@ -1,1 +1,11 @@
-public Driver
+import java.util.*;
+import java.io.*;
+
+public class Driver {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        while (in.hasNextInt()) {
+            System.out.println(Spiral.findDistance(in.nextInt()));
+        }
+    }
+}

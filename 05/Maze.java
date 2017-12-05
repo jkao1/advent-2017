491c70c90c21e429d821d412ad0b8c2caa70090d
Jason Kao <jasonkao85@gmail.com>
2017-12-05 18:50:37 -0500

    rm unnecessaryg

--- 05/Maze.java
+++ 05/Maze.java
@@ -18,19 +18,4 @@ public class Maze {
 				}
 				return steps;
 		}
-
-		public static void printlnInstructions(ArrayList<Integer> instructions, int index)
-		{
-				String output = "[";
-				for (int i = 0; i < instructions.size(); i++) {
-						if (i == index) {
-								output += "(" + instructions.get(i) + ")";
-								index = Integer.MAX_VALUE;
-						} else {
-								output += instructions.get(i);
-						}
-						output += ", ";
-				}
-				System.out.println(output.substring(0, output.length() - 2) + "]");
-		}
 }

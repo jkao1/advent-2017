a9676aaef710fa335d63438475304d54578a322e
Jason Kao <jasonkao85@gmail.com>
2017-12-05 13:04:04 -0500

    day5pt2

--- 05/Maze.java
+++ 05/Maze.java
@@ -7,10 +7,30 @@ public class Maze {
 				int i, steps;
 				i = steps = 0;
 				while (i < instructions.size()) {
-						instructions.set(i, instructions.get(i) + 1);
-						i += instructions.get(i) - 1;
+						if (instructions.get(i) >= 3) {
+								instructions.set(i, instructions.get(i) - 1);
+								i += instructions.get(i) + 1;
+						} else {
+								instructions.set(i, instructions.get(i) + 1);
+								i += instructions.get(i) - 1;
+						}
 						steps++;
 				}
 				return steps;
+		}
+
+		public static void printlnInstructions(ArrayList<Integer> instructions, int index)
+		{
+				String output = "[";
+				for (int i = 0; i < instructions.size(); i++) {
+						if (i == index) {
+								output += "(" + instructions.get(i) + ")";
+								index = Integer.MAX_VALUE;
+						} else {
+								output += instructions.get(i);
+						}
+						output += ", ";
+				}
+				System.out.println(output.substring(0, output.length() - 2) + "]");
 		}
 }

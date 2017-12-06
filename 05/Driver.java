45e00f27011f9ca402d8ce1d1d063751725eae4e
Jason Kao <jasonkao85@gmail.com>
2017-12-05 19:41:12 -0500

    cleaned up day5

--- 05/Driver.java
+++ 05/Driver.java
@@ -2,12 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Driver {
-		public static void main(String[] args) {
-				Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-				ArrayList<Integer> instructions = new ArrayList<>();
-				while (in.hasNextInt()) {
-						instructions.add(in.nextInt());
-				}
-				System.out.println(Maze.countSteps(instructions));
-		}
+  public static void main(String[] args) {
+    Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    
+    ArrayList<Integer> instructions = new ArrayList<>();
+    while (in.hasNextInt()) {
+      
+      instructions.add(in.nextInt());
+    }
+    System.out.println(Maze.countSteps(instructions));
+  }
 }
--- 05/Maze.java
+++ 05/Maze.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 
 public class Maze {
-		public static int countSteps(ArrayList<Integer> instructions)
-		{
-				int i, steps;
-				i = steps = 0;
-				while (i < instructions.size()) {
-						if (instructions.get(i) >= 3) {
-								instructions.set(i, instructions.get(i) - 1);
-								i += instructions.get(i) + 1;
-						} else {
-								instructions.set(i, instructions.get(i) + 1);
-								i += instructions.get(i) - 1;
-						}
-						steps++;
-				}
-				return steps;
-		}
+  public static int countSteps(ArrayList<Integer> instructions)
+  {
+    nt i, steps;
+    i = steps = 0;
+    while (i < instructions.size()) {
+      if (instructions.get(i) >= 3) {
+	instructions.set(i, instructions.get(i) - 1);
+	i += instructions.get(i) + 1;
+      } else {
+	instructions.set(i, instructions.get(i) + 1);
+	i += instructions.get(i) - 1;
+      }
+      steps++;
+    }
+    return steps;
+  }
 }

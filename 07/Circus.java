15daeee6a82bb1160a4f0219c2d1c780692e28c0
Jason Kao <jasonkao85@gmail.com>
2017-12-07 09:43:48 -0500

    getweight

--- 07/Circus.java
+++ 07/Circus.java
@@ -3,12 +3,13 @@ import java.util.regex.Pattern;
 import java.io.*;
 
 public class Circus {
-  public final Pattern PROGRAM_NAME_PATTERN;
+  public final Pattern PROGRAM_NAME_PATTERN, PROGRAM_WEIGHT_PATTERN;
   private Hashtable programs;
 
   public Circus(ArrayList<String> singlePrograms, ArrayList<String> linkedPrograms) {
     programs = new Hashtable();
     PROGRAM_NAME_PATTERN = Pattern.compile("\\w{3,}");
+    PROGRAM_WEIGHT_PATTERN = Pattern.compile("\\d{1,}");
     
     hashSinglePrograms(singlePrograms);
     hashLinkedPrograms(linkedPrograms);
@@ -29,17 +30,35 @@ public class Circus {
     for (String program : singlePrograms) {
       Scanner scan = new Scanner(program);
       String name = scan.next();
-      int weight = scan.nextInt();
+      int weight = getWeight(program);
       programs.put(name, new Program(name, weight));
     }
   }
 
+  private int getWeight(String program) {
+    String weight = "";
+    boolean scanningInt = false;
+    for (int i = 0; i < program.length(); i++) {
+      if (scanningInt) {
+	if (program.charAt(i) == ')') {
+	  break;
+	}
+	weight += program.charAt(i);
+      } else {
+	if (program.charAt(i) == '(') {
+	  scanningInt = true;
+	}
+      }
+    }
+    return Integer.parseInt(weight);
+  }
+	
   private void hashLinkedPrograms(ArrayList<String> linkedPrograms) {
     // format of program string: ozfsktz (56) -> xzwjii, uhxjy
     for (String program : linkedPrograms) {
       Scanner scan = new Scanner(program);
       String name = scan.next();
-      int weight = scan.nextInt();
+      int weight = Integer.parseInt(scan.findInLine(PROGRAM_WEIGHT_PATTERN));
       Program p = new Program(name, weight);
       while (scan.hasNext(PROGRAM_NAME_PATTERN)) {
 	Program child = (Program) (programs.get(scan.next(PROGRAM_NAME_PATTERN)));

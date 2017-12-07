9fdf7a87f07e63b5640f1d3d7aded4a626289c9c
Jason Kao <jasonkao85@gmail.com>
2017-12-07 03:54:08 -0500

    circus

--- 07/Circus.java
+++ 07/Circus.java
@@ -1,2 +1,63 @@
+import java.util.*;
+import java.util.regex.Pattern;
+import java.io.*;
+
 public class Circus {
+  public final Pattern PROGRAM_NAME_PATTERN;
+  private Hashtable programs;
+
+  public Circus(ArrayList<String> singlePrograms, ArrayList<String> linkedPrograms) {
+    programs = new Hashtable();
+    PROGRAM_NAME_PATTERN = Pattern.compile("\w{3,}");
+    
+    hashSinglePrograms(singlePrograms);
+    hashLinkedPrograms(linkedPrograms);
+  }
+  
+  public String findBottomProgram() {
+    for (Object program : programs.values()) {
+      Program p = (Program) program;
+      if (p.children.size() == 0) {
+	return p.name;
+      }
+    }
+    return "";
+  }
+
+  private void hashSinglePrograms(ArrayList<String> singlePrograms) {    
+    // format of program string: yvpwz (50)
+    for (String program : singlePrograms) {
+      Scanner scan = new Scanner(program);
+      String name = scan.next();
+      int weight = scan.nextInt();
+      programs.put(name, new Program(name, weight));
+    }
+  }
+
+  private void hashLinkedPrograms(ArrayList<String> linkedPrograms) {
+    // format of program string: ozfsktz (56) -> xzwjii, uhxjy
+    for (String program : linkedPrograms) {
+      Scanner scan = new Scanner(program);
+      String name = scan.next();
+      int weight = scan.nextInt();
+      Program p = new Program(name, weight);
+      while (scan.hasNext(PROGRAM_NAME_PATTERN)) {
+	Program child = (Program) (programs.get(scan.next(PROGRAM_NAME_PATTERN)));
+	p.children.add(child);
+      }
+      programs.put(name, p);
+    }
+  }
+
+  private class Program {
+    public ArrayList<Program> children;
+    public String name;
+    public int weight;
+
+    public Program(String name, int weight) {
+      children = new ArrayList<>();
+      this.name = name;
+      this.weight = weight;
+    }
+  }
 }
--- 07/Driver.java
+++ 07/Driver.java
@@ -4,10 +4,18 @@ import java.io.*;
 public class Driver {
   public static void main(String[] args) {
     Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    ArrayList<String> singlePrograms = new ArrayList<>();
+    ArrayList<String> linkedPrograms = new ArrayList<>();
     while (in.hasNextLine()) {
-      // do stuff
+      String line = in.nextLine();
+      if (line.contains("->")) {
+	linkedPrograms.add(line);
+      } else {
+	singlePrograms.add(line);
+      }
     }
-    System.out.println(Circus...);
+    Circus c = new Circus(singlePrograms, linkedPrograms);
+    System.out.println(c.findBottomProgram());
   }
 }
   

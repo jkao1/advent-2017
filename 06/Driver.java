3c064aa1ca7d1742703cb9fd2f8d680d537583f4
Jason Kao <jasonkao85@gmail.com>
2017-12-07 00:57:56 -0500

    too slow

--- 06/Driver.java
+++ 06/Driver.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.io*;
+import java.io.*;
 
 public class Driver {
   public static void main(String[] args) {
--- 06/Memory.java
+++ 06/Memory.java
@@ -4,11 +4,32 @@ public class Memory {
   public static int redistribute(ArrayList<Integer> bank) {
     Hashtable hashtable = new Hashtable();
     while (!hashtable.containsKey(bank.toString())) {
+      System.out.println(bank);
+      hashtable.put(bank.toString(), "placeholder");
       int maxIndex = getMaxIndex(bank);
+
+      int toRedistribute = bank.get(maxIndex) % (bank.size() - 1);
+      int chunk = toRedistribute / (bank.size() - 1);
+      for (int i = 0; i < bank.size(); i++) {
+	if (toRedistribute == 0 && chunk == 0) {
+	  break;
+	}
+	if (i == maxIndex) {
+	  bank.set(i, bank.get(i) - toRedistribute);
+	} else {
+	  if (toRedistribute > 0) {
+	    bank.set(i, bank.get(i) + chunk + 1);
+	    toRedistribute--;
+	  } else {
+	    bank.set(i, bank.get(i) + chunk);
+	  }
+	}
+      }
     }
+    return hashtable.size();
   }
 
-  private static int getMatIndex(ArrayList<Integer> array) {
+  private static int getMaxIndex(ArrayList<Integer> array) {
     int max, maxIndex;
     max = maxIndex = 0;
     for (int i = 0; i < array.size(); i++) {

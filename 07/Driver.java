6fbdb560379c295524cb8a440c2bc95ed9669df6
Jason Kao <jasonkao85@gmail.com>
2017-12-07 01:52:55 -0500

    adventday7

---
+++ 07/Driver.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    while (in.hasNextLine()) {
+      // do stuff
+    }
+    System.out.println(Circus...);
+  }
+}
+  

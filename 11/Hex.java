c4d4320231eec2352087b8f754bd9e57d63cd575
Jason Kao <jasonkao85@gmail.com>
2017-12-11 16:02:37 -0500

    day11pt2

--- 11/Hex.java
+++ 11/Hex.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Hex {
   public static int findFewestSteps(String[] directions) {
     int hexesUp, hexesRight;
     hexesUp = hexesRight = 0;
+    ArrayList<Integer> answers = new ArrayList<>();
     for (String s : directions) {
       if (s.length() == 2) {
         hexesUp += directionToValue(s.charAt(0));
@@ -9,7 +12,19 @@ public class Hex {
       } else if (s.length() == 1) { // n or s
         hexesUp += directionToValue(s.charAt(0)) * 2;
       }
+      answers.add(calculateFewest(hexesUp, hexesRight));
     }
+
+    int max = 0;
+    for (int ans : answers) {
+      if (ans > max) {
+        max = ans;
+      }
+    }
+    return max;
+  }
+
+  private static int calculateFewest(int hexesUp, int hexesRight) {
     int ans;
     if (hexesUp == hexesRight) {
       ans = hexesUp;

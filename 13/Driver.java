a7950dabaa490d3db8c8dd83e4f9c3008ee0f564
Jason Kao <jasonkao85@gmail.com>
2017-12-12 23:59:07 -0500

    13

---
+++ 13/Driver.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    while (in.hasNext()) {
+      in.next();
+    }
+    System.out.println();
+  }
+}

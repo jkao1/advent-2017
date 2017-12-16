654774a554c866b7d55e112f0b28db61e12a4a86
Jason Kao <jasonkao85@gmail.com>
2017-12-16 16:26:04 -0500

    forgot disk

---
+++ 14/Disk.java
@@ -0,0 +1,9 @@
+public class Disk {
+  public static int findDisksUsed(String in) {
+    int ans = 0;
+    for (int i = 0; i < 128; i++) {
+      String hashInput = in + "-" + i;
+      
+    }
+  }
+}
